8a535097f188e50b4a114652ca414839420d5890
zodiakara <114649190+zodiakara@users.noreply.github.com>
2022-12-08 09:42:45 +0100

    typescript-react app initialization

---
+++ ts-react/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import ClassComponent from "./components/ClassComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ClassComponent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
